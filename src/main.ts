90b1a0ad47de484afc1ca21b249eb25aebc491db
JinGoo <zzgg3040@naver.com>
2025-05-19 06:29:30 +0900

    chore: add-port-log

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(process.env.PORT);
   await app.listen(process.env.PORT || 4000);
 }
 
