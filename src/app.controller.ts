341199a951c1454d5fcc178daa29d6c704fc5312
JinGoo <zzgg3040@naver.com>
2025-05-19 05:07:33 +0900

    feat: get-port-env

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -5,8 +5,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('env')
   getHello(): string {
-    return this.appService.getHello();
+    return process.env.UPSTREAM_COLOR || 'local';
   }
 }
