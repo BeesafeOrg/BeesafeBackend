8b2ca5e17cd95c4ae3c0cd27bfb9c8a70593c157
JinGoo <zzgg3040@naver.com>
2025-05-19 05:25:43 +0900

    chore: fix-port-env

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as process from 'node:process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT || 4000);
 }
 
 void bootstrap();
