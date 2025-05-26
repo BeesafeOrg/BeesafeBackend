7929df81653b080b6d4051a79549ac6f7a618b69
JinGoo <zzgg3040@naver.com>
2025-05-27 01:18:50 +0900

    feat: get-mypage-info

--- src/domain/member/dto/create-member.dto.ts
+++ src/domain/member/dto/create-member.dto.ts
@@ -1,12 +1,5 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
-
 export class CreateMemberDto {
-  @IsNotEmpty()
   nickname: string;
-
-  @IsEmail()
   email: string;
-
-  @IsNotEmpty()
   profileImageUrl: string;
 }
--- src/domain/member/member.controller.ts
+++ src/domain/member/member.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { MemberService } from './member.service';
 import { JwtAccessGuard } from '../auth/guards/jwt-access.guard';
+import { MemberResponseDto } from './dto/member-response.dto';
 
 @Controller('members')
 export class MemberController {
@@ -8,7 +9,16 @@ export class MemberController {
 
   @Get('me')
   @UseGuards(JwtAccessGuard)
-  testMe(@Req() req: any) {
-    return req.user.memberId;
+  async getMyInfo(@Req() req: any): Promise<MemberResponseDto> {
+    const member = await this.memberService.findByIdOrThrowException(
+      req.user.memberId,
+    );
+    return {
+      memberId: member.id,
+      email: member.email,
+      nickname: member.nickname,
+      profileImageUrl: member.profileImageUrl,
+      role: member.role,
+    };
   }
 }
