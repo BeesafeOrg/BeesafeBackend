ee9e97671cf909b76907e5d14a48161c1a05800d
JinGoo <zzgg3040@naver.com>
2025-05-31 05:17:05 +0900

    feat: init-swagger-config

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { SuccessInterceptor } from './common/interceptors/success.interceptor';
 import { RequestLoggerInterceptor } from './common/interceptors/request-logger.interceptor';
 import { HttpExceptionFilter } from './common/filters/exception/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,20 @@ async function bootstrap() {
   app.useGlobalInterceptors(new RequestLoggerInterceptor());
 
   app.useGlobalFilters(new HttpExceptionFilter());
+
+  const config = new DocumentBuilder()
+    .setTitle('Beesafe API')
+    .setDescription('Beesafe API Docs')
+    .setVersion('1.0')
+    .addTag('android-mobile-programming-team-1')
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'jwt-access',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(4000);
 }
